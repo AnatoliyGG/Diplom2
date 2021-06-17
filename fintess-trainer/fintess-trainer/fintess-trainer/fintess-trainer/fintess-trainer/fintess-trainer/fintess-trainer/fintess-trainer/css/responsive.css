0b776eba00ca936c4f0f67c77a376b71eb3ed3d2
AnatoliyGG <tolik-mazkov@mail.ru>
2021-06-18 00:45:44 +0300

    first commit

---
+++ fintess-trainer/fintess-trainer/fintess-trainer/fintess-trainer/fintess-trainer/fintess-trainer/fintess-trainer/fintess-trainer/css/responsive.css
@@ -0,0 +1,348 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<html lang="en" class="no-js">
+
+<head>
+    <!--- Basic Page Needs  -->
+    <meta charset="utf-8">
+    <title>Fitness Trainer</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="keywords" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Mobile Specific Meta  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- Google Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700|Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/preview.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/png" href="img/favicon.ico">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+</head>
+
+<body data-spy="scroll" data-target="#scroll-menu" data-offset="100">
+    <!-- Preloader -->
+    <div class="preloader-wrap">
+        <div class="preloader-inside">
+            <div class="preloader">
+                <div class="preloader-inner box1"></div>
+                <div class="preloader-inner box2"></div>
+                <div class="preloader-inner box3"></div>
+            </div>
+        </div>
+    </div>
+    <!-- End Preloader -->
+    <!-- Header Section -->
+    <header id="home">
+        <div class="full-wrap hero-wrap">
+            <div class="hero-inner">
+                <div class="hero-wrap-inside">
+                    <h1>Fitness Trainer</h1>
+                    <span>Responsive Bootstrap Template</span>
+                    <div class="purchase-button">
+                        <a href="https://themeforest.net/item/fitnesstrainer-responsive-bootstrap-template/19873816" class="btn btn-default btn-orange" target="_blank"><i class="fa fa-shopping-cart"></i> | Purchase Now</a>
+                        <a class="btn btn-default btn-orange smoothscroll" href="#page_layout"><i class="fa fa-link"></i> | View Demo</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Page layout Section -->
+    <div class="page-layout" id="page_layout">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="layout-title">
+                        <h2>ALL HOME PAGE LAYOUT'S</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 text-center">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/index.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/home.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/index.html" target="_blank">HOME</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/about-us.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/about.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/about-us.html" target="_blank">ABOUT us</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/blog.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/blog.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/blog.html" target="_blank">BLOG PAGE</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/blog-details.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/blog-details.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/blog-details.html" target="_blank">BLOG DETAIALS</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/event.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/event.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/event.html" target="_blank">EVENT</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/event-details.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/event-details.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/event-details.html" target="_blank">EVENT DETAIALS</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/pricing-table.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/pricing-table.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/pricing-table.html" target="_blank">PRICING TABLE</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/shop.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/shop.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/shop.html" target="_blank">SHOP</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/product-single-page.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/product-single.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/product-single-page.html" target="_blank">PRODUCT SINGLE</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/success-story.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/success-story.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/success-story.html" target="_blank">SUCCESS STORY</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/success-story-details.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/success-story-details.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/success-story-details.html" target="_blank">SUCCESS STORY DETAIALS</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/about-trainer.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/about-trainer.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/about-trainer.html" target="_blank">ABOUT TRAINER</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/contact.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/contact.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/contact.html" target="_blank">CONTACT</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/my-account.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/my-account.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/my-account.html" target="_blank">MY ACCOUNT</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/exercise-plan.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/exercise-plan.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/exercise-plan.html" target="_blank">EXERCISE PLAN</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/exercise-plan-details.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/exercise-plan-details.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/exercise-plan-details.html" target="_blank">EXERCISE PLAN DETAIALS</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/forgot-password.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/forgot.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/forgot-password.html" target="_blank">FORGOT PASSWORD</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/login.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/login.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/login.html" target="_blank">LOGIN</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-4">
+                            <div class="single-preview">
+                                <div class="preview-content">
+                                    <a href="fintess-trainer/registration.html" target="_blank">
+                                        <div class="preview-content-thumb" style="background-image: url(img/registration.png)"></div>
+                                    </a>
+                                </div>
+                                <div class="thumb-title">
+                                    <h2><a href="fintess-trainer/registration.html" target="_blank">REGISTRATION</a></h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page layout Section Ends -->
+    <div class="preview-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 text-center">
+                    <h3 class="preview-footer-title">
+                            BOOST YOUR WEBSITE WITH OUR MULTIPURPOSE TEMPALTE
+                        </h3>
+                    <h4><a href="https://themeforest.net/item/fitnesstrainer-responsive-bootstrap-template/19873816" target="_blank" class="btn btn-dfault btn-preview"><i class="fa fa-shopping-cart"></i> | PURCHASE NOW!</a></h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Contact Section -->
+    <!-- Scripts -->
+    <script src="js/jquery-1.12.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/theme.js"></script>
+</body>
+
+</html>
